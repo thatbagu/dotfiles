25b1ea65e5c5def69266c8d72602d0b021d24b4b
Jahysama <no-reply@example.com>
2025-03-15 23:57:29 +0500

    fix tags on homelab nodes for longhorn

--- modules/system/k3s/default.nix
+++ modules/system/k3s/default.nix
@@ -1,8 +1,6 @@
 { pkgs, lib, config, username, hostname, ... }:
-
 with lib;
 let cfg = config.modules.k3s;
-
 in {
   options.modules.k3s = {
     enable = mkEnableOption "k3s";
@@ -19,38 +17,40 @@ in {
   };
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [ k3s cifs-utils nfs-utils ];
-
     systemd.tmpfiles.rules =
       [ "L+ /usr/local/bin - - - - /run/current-system/sw/bin/" ];
     virtualisation.docker.logDriver = "json-file";
-
     networking.firewall.enable = mkForce false;
-
     services = {
       openssh.enable = mkForce true;
-
       openiscsi = {
         enable = true;
         name = "iqn.2016-04.com.open-iscsi:${hostname}";
       };
-
       k3s = {
         enable = true;
         role = if cfg.master then "server" else "agent";
         tokenFile = config.sops.secrets.k3s_token.path;
-      } // (if cfg.master then {
-        # Server (master) specific configuration
-        extraFlags = toString [
+        # Common node label configuration for both server and agent
+        extraFlags = toString ((if cfg.master then [
+          # Server (master) specific configuration
           "--cluster-init"
           "--write-kubeconfig-mode=0644"
           "--disable=servicelb"
           "--disable=traefik"
           "--disable=local-storage"
-        ];
-      } else {
-        # Agent (worker) specific configuration
+        ] else
+          [
+            # Agent (worker) specific configuration
+          ]) ++ [
+            # Common flags for both server and agent
+            "--node-label=node.longhorn.io/create-default-disk=true"
+          ]);
+      } // (if !cfg.master then {
+        # Agent-only configuration
         serverAddr = "https://${cfg.masterHostname}:6443";
-      });
+      } else
+        { });
     };
   };
 }
