6b66de51a295e79138aea28cd0fc4c51953600d8
Jahysama <no-reply@example.com>
2025-05-17 22:26:54 +0500

    Add custom greetings to fish shell
    - Create a fish_greeting function with time-based greetings
    
    - Add day-specific messages for Monday and Friday
    
    - Include ASCII art cat in the greeting
    
    - Display system information like OS and uptime
    
    - Use colorful output for better visual appeal
    
    Signed-off-by: Jahysama <megametagross@outlook.de>

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, config, ... }:
+{ lib, config, ... }:
 with lib;
 let cfg = config.modules.fish;
 in {
@@ -30,6 +30,85 @@ in {
         # Run the function once at startup to load the token
         if test -n "$GITHUB_TOKEN_PATH" -a -r "$GITHUB_TOKEN_PATH"
             set -gx GITHUB_TOKEN (cat $GITHUB_TOKEN_PATH)
+        end
+
+        # Custom greeting function
+        function fish_greeting
+            set -l hour (date +%H)
+            set -l day (date +%u)
+            set -l hostname (hostname)
+            set -l username $USER
+            
+            # ASCII art for the greeting
+            set -l ascii_art "
+            |\---/|
+            | o_o |
+             \_^_/
+            "
+            
+            # Time-based greetings
+            if test $hour -ge 5 -a $hour -lt 12
+                set -l morning_greetings \
+                    "Good morning, $username! Ready to conquer the day?" \
+                    "Rise and shine, $username! The code awaits." \
+                    "Morning has broken! Time to break some code." \
+                    "Top of the morning to you, $username!"
+                set greeting (random choice $morning_greetings)
+            else if test $hour -ge 12 -a $hour -lt 18
+                set -l afternoon_greetings \
+                    "Good afternoon, $username! How's your day going?" \
+                    "Afternoon delight! Time for some coding magic." \
+                    "The day is still young, $username. What will you accomplish?" \
+                    "Afternoon, $username! Coffee time?"
+                set greeting (random choice $afternoon_greetings)
+            else if test $hour -ge 18 -a $hour -lt 22
+                set -l evening_greetings \
+                    "Good evening, $username! Productive day?" \
+                    "Evening has arrived. Time for the real work to begin!" \
+                    "The night is still young, $username." \
+                    "Evening, $username! Dinner before code or code before dinner?"
+                set greeting (random choice $evening_greetings)
+            else
+                set -l night_greetings \
+                    "Working late, $username? Don't forget to rest!" \
+                    "The night owl catches the bug." \
+                    "Midnight coding session? Remember to blink occasionally." \
+                    "Burning the midnight oil, $username?"
+                set greeting (random choice $night_greetings)
+            end
+            
+            # Day-specific messages for Friday and Monday
+            if test $day -eq 5
+                set -l friday_messages \
+                    "It's Friday! Weekend is approaching!" \
+                    "Friday vibes! Almost there!" \
+                    "Happy Friday, $username!"
+                set friday_msg (random choice $friday_messages)
+                set greeting "$greeting $friday_msg"
+            else if test $day -eq 1
+                set -l monday_messages \
+                    "It's Monday! Let's start the week strong!" \
+                    "New week, new opportunities!" \
+                    "Monday: A fresh start!"
+                set monday_msg (random choice $monday_messages)
+                set greeting "$greeting $monday_msg"
+            end
+            
+            # Display the greeting with some color
+            echo $ascii_art
+            set_color brgreen
+            echo "Welcome to $hostname!"
+            set_color yellow
+            echo $greeting
+            set_color normal
+            
+            # Show system info
+            echo ""
+            set_color cyan
+            echo "System: "(uname -rs)
+            echo "Uptime: "(uptime | cut -d ',' -f1 | cut -d ' ' -f4-)
+            set_color normal
+            echo ""
         end
 
         # Improve contrast for command line text
