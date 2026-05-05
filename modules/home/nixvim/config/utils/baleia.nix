68e6af5a296f927f965b8cbc1b0964841ef4dca2
Jahysama <megametagross@outlook.de>
2026-05-05 12:06:20 +0500

    remove dump highlighting attempts

--- modules/home/nixvim/config/utils/baleia.nix
+++ modules/home/nixvim/config/utils/baleia.nix
@@ -1,23 +1,26 @@
 { lib, config, ... }: {
   options = {
-    baleia.enable = lib.mkEnableOption "Enable terminal rendering for .dump files";
+    baleia.enable = lib.mkEnableOption "Enable log highlighting for .dump files";
   };
   config = lib.mkIf config.baleia.enable {
     extraConfigLua = ''
-      vim.api.nvim_create_autocmd("BufReadPost", {
+      vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
         pattern = { "*.dump" },
-        callback = function(args)
-          local filepath = vim.api.nvim_buf_get_name(args.buf)
-          local orig_buf = args.buf
-          vim.cmd("enew")
-          vim.fn.termopen("cat " .. vim.fn.shellescape(filepath), {
-            on_exit = function()
-              vim.schedule(function()
-                vim.cmd("stopinsert")
-              end)
-            end,
-          })
-          vim.api.nvim_buf_delete(orig_buf, { force = true })
+        callback = function()
+          vim.cmd([[
+            syntax match DumpError   /\c\<\(error\|fail\|failed\|fatal\|critical\)\>/
+            syntax match DumpWarn    /\c\<\(warn\|warning\)\>/
+            syntax match DumpOk      /\c\<\(ok\|success\|done\|passed\)\>/
+            syntax match DumpNumber  /\b[0-9]\+\(\.[0-9]\+\)\?\b/
+            syntax match DumpPath    /\~\?\/[a-zA-Z0-9_.\/\-]*/
+            syntax match DumpString  /"\([^"]*\)"/
+            highlight DumpError  guifg=#ff5555 ctermfg=Red
+            highlight DumpWarn   guifg=#ffb86c ctermfg=Yellow
+            highlight DumpOk     guifg=#50fa7b ctermfg=Green
+            highlight DumpNumber guifg=#bd93f9 ctermfg=Magenta
+            highlight DumpPath   guifg=#6272a4 ctermfg=Blue
+            highlight DumpString guifg=#f1fa8c ctermfg=Yellow
+          ]])
         end,
       })
     '';
