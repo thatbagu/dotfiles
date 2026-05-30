6fa2041c6ad7e5dbded39029a8c4aeed7504f388
Jahysama <megametagross@outlook.de>
2026-05-30 23:31:06 +0500

    homelabs keys stuff

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -94,6 +94,13 @@
     shell = pkgs.fish;
   };
 
+  services.openssh = {
+    hostKeys = [
+      { path = "/persist/etc/ssh/ssh_host_ed25519_key"; type = "ed25519"; }
+      { path = "/persist/etc/ssh/ssh_host_rsa_key"; type = "rsa"; bits = 4096; }
+    ];
+  };
+
   programs.ssh = {
     extraConfig = ''
       Host *
--- modules/system/impermanence/default.nix
+++ modules/system/impermanence/default.nix
@@ -66,11 +66,6 @@
       # System configuration
       "/etc/adjtime"
 
-      # SSH host keys
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
-      "/etc/ssh/ssh_host_rsa_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
 
 
       # # Security and access control
