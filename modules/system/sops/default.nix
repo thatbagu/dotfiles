399d3dc3e3119013a21f952f78737d13e0d37fcb
Jahysama <megametagross@outlook.de>
2026-05-31 03:03:32 +0500

    onboard new user of wireguard

--- modules/system/sops/default.nix
+++ modules/system/sops/default.nix
@@ -1,9 +1,19 @@
-{ pkgs, lib, config, username, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  username,
+  ...
+}:
 
 with lib;
-let cfg = config.modules.sops;
-in {
-  options.modules.sops = { enable = mkEnableOption "sops"; };
+let
+  cfg = config.modules.sops;
+in
+{
+  options.modules.sops = {
+    enable = mkEnableOption "sops";
+  };
 
   config = mkIf cfg.enable {
     sops = {
@@ -11,14 +21,30 @@ in {
       defaultSopsFile = ./secrets.yaml;
       defaultSopsFormat = "yaml";
       secrets = {
-        antropic_key = { owner = "${username}"; };
-        spotify_client_id = { owner = "${username}"; };
-        github_token = { owner = "${username}"; };
-        git_email = { owner = "${username}"; };
-        cloudflare_email = { owner = "${username}"; };
-        k3s_token = { owner = "${username}"; };
-        pihole_password = { owner = "${username}"; };
-        cloudflare_token = { owner = "${username}"; };
+        antropic_key = {
+          owner = "${username}";
+        };
+        spotify_client_id = {
+          owner = "${username}";
+        };
+        github_token = {
+          owner = "${username}";
+        };
+        git_email = {
+          owner = "${username}";
+        };
+        cloudflare_email = {
+          owner = "${username}";
+        };
+        k3s_token = {
+          owner = "${username}";
+        };
+        pihole_password = {
+          owner = "${username}";
+        };
+        cloudflare_token = {
+          owner = "${username}";
+        };
 
         # WireGuard server secrets
         wireguard_server_private_key = {
@@ -44,7 +70,18 @@ in {
           mode = "0600";
         };
 
-        user_password = { neededForUsers = true; };
+        secrets.dsh_wg_public_key = {
+          owner = "root";
+          mode = "0644";
+        };
+        secrets.dsh_wg_private_key = {
+          owner = "${username}";
+          mode = "0600";
+        };
+
+        user_password = {
+          neededForUsers = true;
+        };
 
         private_ssh_key = {
           path = "/home/${username}/.ssh/ssh_host_ed25519_key";
