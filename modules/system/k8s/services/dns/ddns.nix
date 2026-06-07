ad7d5205853c60cf981ad5295d1689c2aebb184e
Jahysama <megametagross@outlook.de>
2026-06-07 17:04:43 +0500

    make nextcloud vpn-only, remove from cloudflare ddns

--- modules/system/k8s/services/dns/ddns.nix
+++ modules/system/k8s/services/dns/ddns.nix
@@ -67,7 +67,6 @@ let
 
             update_record "vpn.${vars.domain}" false
             update_record "signal.${vars.domain}" false
-            update_record "nextcloud.${vars.domain}" true
           ''];
           volumeMounts = [{
             name = "cf-token";
